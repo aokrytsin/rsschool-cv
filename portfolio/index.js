ca65f62a1d6605f0e63e10ece1c7e26d3a89136b
aokrytsin <aokrytsin@gmail.com>
2022-01-24 16:26:34 +0500

    fix: hover color for hamburger and menus

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger-lines');
-const nav = document.querySelector('.nav-list');
+const nav = document.querySelector('.nav-list-burger');
 
 function toggleMenu() {
   hamburger.classList.toggle('is-active');
