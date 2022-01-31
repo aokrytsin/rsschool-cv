51e1a739f0cd02832f96f57119851ba8144e6377
aokrytsin <aokrytsin@gmail.com>
2022-01-31 20:38:44 +0500

    fix: some js and css code

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,4 @@
-//================================= ! Hamburger
+//*================================= Hamburger
 const hamburger = document.querySelector('.hamburger-lines');
 const nav = document.querySelector('.nav-list-burger');
 
@@ -18,22 +18,55 @@ function closeMenu(event) {
         nav.classList.remove('is-active');
     }
 }
-//================================= ! Portfolio 
+//*================================= Portfolio 
 const portfolioImages = document.querySelectorAll('.portfolio-img');
 const portfolioBtns = document.querySelector('.portfolio-btn-section');
 
 function changeImage(event) {
   if(event.target.classList.contains('portfolio-btn')) {
-    if (event.target.dataset.season == "winter") {
+    if (event.target.dataset.season == 'winter') {
       portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`);
-    } if (event.target.dataset.season == "spring") {
+    } if (event.target.dataset.season == 'spring') {
       portfolioImages.forEach((img, index) => img.src = `./assets/img/spring/${index + 1}.jpg`);
-    } if (event.target.dataset.season == "summer") {
+    } if (event.target.dataset.season == 'summer') {
       portfolioImages.forEach((img, index) => img.src = `./assets/img/summer/${index + 1}.jpg`);
-    } if (event.target.dataset.season == "autumn") {
+    } if (event.target.dataset.season == 'autumn') {
       portfolioImages.forEach((img, index) => img.src = `./assets/img/autumn/${index + 1}.jpg`);
     }
   }
 }
 
 portfolioBtns.addEventListener('click', changeImage);
+//*================================= Pre-load
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+function preloadImagesEach() {
+  for(let i = 1; i <= 6; i++) {
+    const img = new Image();
+    img.src = `./assets/img/${seasons}/${i}.jpg`;
+  }
+}
+
+//*================================= Active-Btn
+const portfolioBtn = document.querySelectorAll('.portfolio-btn');
+
+function changeClassActive() {
+  if(event.target.classList.contains('portfolio-btn')) {
+    portfolioBtn.classList.add('active');
+  }
+}
+portfolioBtn.addEventListener('click', changeClassActive);
+
+function changeClassNonActive() {
+  portfolioBtn.classList.remove('active');
+}
+
+/*
+let page = document.querySelector('.class');
+let themeButton = document.querySelector('.class');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
+*/
